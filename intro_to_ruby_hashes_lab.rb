b4bb2ab2edff29d4a27b09ac04a02434c95a430c
Romario Gustave <romagustave10@gmail.com>
2019-10-11 18:54:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,8 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-  people={name: google, age: 30}
-	{}
+  people={:name "google"}
+  puts people
 end
 
 def monopoly_with_second_tier
