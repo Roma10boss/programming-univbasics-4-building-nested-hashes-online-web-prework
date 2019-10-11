d5b3801590b56eb2bd5fce26b07f1c8df5087518
Romario Gustave <romagustave10@gmail.com>
2019-10-11 18:57:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,6 +21,8 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+   people={:name "google"}
+  puts people
 end
 
 def monopoly_with_fourth_tier
@@ -28,4 +30,6 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+   people={:name "google"}
+  puts people
 end
